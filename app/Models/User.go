e5f7741259c35f48c0283749dad1913078558f8f
Anh Ngo Q <anh.ngo@asiantech.vn>
2018-03-29 12:24:57 +0700

    fix dev branch

--- app/Models/User.go
+++ app/Models/User.go
@@ -6,7 +6,6 @@ import (
 
 type User struct {
 	gorm.Model
-	Id         uint32 `gorm:"primary_key"`
 	Name       string `sql:"type:text"`
 	City       string `sql:"type:text"`
 	IdentityId int64  `sql:"type:integer"`
