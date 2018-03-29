81433bb6b49f7138125895ae947b5a430c76c07e
at-trampham <tram.pham@asiantech.vn>
2018-03-29 09:48:23 +0700

    validation form create

--- app/Http/Handlers/UserHandler.go
+++ app/Http/Handlers/UserHandler.go
@@ -1,19 +1,25 @@
 package Handlers
 
 import (
-
-	// . "go-t1/app/Models"
+	validate "go-t1/app/Http/Validate"
 	"html/template"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
+//
 type UserHandler struct {
 	repo UserRepository
 }
 
 func NewUserHandler(repo UserRepository) *UserHandler {
 	return &UserHandler{repo}
+}
+
+//
+type error interface {
+	Error() string
 }
 
 //
@@ -49,7 +55,25 @@ func (controller UserHandler) Store(w http.ResponseWriter, r *http.Request) {
 	city := r.FormValue("city")
 	identityID, _ := strconv.ParseInt(r.FormValue("identity-id"), 10, 64)
 	gender, _ := strconv.ParseBool(r.FormValue("gender"))
-	controller.repo.InsertUser(name, city, identityID, gender)
 
-	http.Redirect(w, r, "/", 301)
+	// validate database
+	user := validate.User{
+		Name:       name,
+		City:       city,
+		IdentityID: r.FormValue("identity-id"),
+		Gender:     gender,
+	}
+	error := user.Validate()
+	if error != nil {
+		m := make(map[string]string)
+		message := strings.Split(error.Error(), ";")
+		for _, value := range message {
+			msg := strings.Split(strings.Trim(value, " "), ":")
+			m[msg[0]] = value
+		}
+		validate.Render(w, "app/Views/Users/create.html", m)
+	} else {
+		controller.repo.InsertUser(name, city, identityID, gender)
+		http.Redirect(w, r, "/", 301)
+	}
 }
---
+++ app/Http/Validate/User.go
@@ -0,0 +1,37 @@
+package Validate
+
+import (
+	"html/template"
+	"net/http"
+	"regexp"
+
+	"github.com/go-ozzo/ozzo-validation"
+)
+
+//
+type User struct {
+	Name       string
+	City       string
+	IdentityID string
+	Gender     bool
+}
+
+func (a User) Validate() error {
+	return validation.ValidateStruct(&a,
+		validation.Field(&a.Name, validation.Required),
+		validation.Field(&a.City, validation.Required),
+		validation.Field(&a.IdentityID, validation.Required, validation.Match(regexp.MustCompile("^[0-9]{9}$")).Error("Must be a string with night digits")),
+		validation.Field(&a.Gender, validation.Required),
+	)
+}
+
+//
+func Render(w http.ResponseWriter, filename string, data interface{}) {
+	tmpl, err := template.ParseFiles(filename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	if err := tmpl.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
